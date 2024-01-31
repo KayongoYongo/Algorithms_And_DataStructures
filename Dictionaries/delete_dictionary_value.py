3f77e02de84e9535f6a9dbb51dd0d344186e6104
KayongoYongo <sknsammie10@gmail.com>
2024-01-31 23:15:45 +0000

    delete element

---
+++ Dictionaries/delete_dictionary_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# To delete an entry, use the del statement, specifying the key to delete:
+
+champions_league = {
+    'England':'Manchester City',
+    'Spain':'Barcelona',
+    'Italy':'Inter Milan',
+    'Germany':'Borrusia Dortmound',
+    'France':'Lens'
+    }
+
+del champions_league['France']
+print(champions_league)
