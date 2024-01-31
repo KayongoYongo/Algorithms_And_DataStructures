7279825cd76647897b9f386a3bbd74b26d8e6501
KayongoYongo <sknsammie10@gmail.com>
2024-01-31 23:09:51 +0000

    add and update values

---
+++ Dictionaries/adding_dictionary_values.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+champions_league = {
+    'England':'Manchester City',
+    'Spain':'Barcelona',
+    'Italy':'Inter Milan',
+    'Germany':'Borrusia Dortmound',
+    'France':'Lens'
+}
+
+# Adding an entry to an existing dictionary is simply a matter of assigning a new key and value:
+champions_league['Portugal'] = 'Porto'
+
+print(champions_league)
---
+++ Dictionaries/update_dictionary_values.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# If you want to update an entry, you can just assign a new value to an existing key:
+
+champions_league = {
+    'England':'Manchester City',
+    'Spain':'Barcelona',
+    'Italy':'Inter Milan',
+    'Germany':'Borrusia Dortmound',
+    'France':'Lens'
+}
+
+champions_league['England'] = 'Liverpool'
+print(champions_league)
